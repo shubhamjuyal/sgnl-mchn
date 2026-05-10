8652596cb2dfa1594a915cda4d6780031bfec8d1
shubham juyal <shubhamjuyal4444@gmail.com>
2026-05-11 03:37:14 +0530

    Update middleware.ts

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -17,5 +17,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/|favicon.ico|.*\\.(?:png|jpg|jpeg|gif|svg|webp|ico)$).*)"],
+  matcher: ["/((?!api/|_next/|favicon.ico|.*\\.(?:png|jpg|jpeg|gif|svg|webp|ico)$).*)"],
 };
